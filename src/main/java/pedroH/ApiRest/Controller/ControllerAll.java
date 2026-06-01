02fb7759d9268f1ae69ddd6fad1c88c6faedaa93
Pedrofaustz <pedrofaustz17@gmail.com>
2026-06-01 16:19:34 -0300

    Tratamento de Erros HTTP

--- src/main/java/pedroH/ApiRest/Controller/ControllerAll.java
+++ src/main/java/pedroH/ApiRest/Controller/ControllerAll.java
@@ -1,11 +1,13 @@
 package pedroH.ApiRest.Controller;
 
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pedroH.ApiRest.Entity.Usuario;
 import pedroH.ApiRest.Repository.UserRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -22,34 +24,46 @@ public class ControllerAll {
     }
 
     @PostMapping("/criar")
-    public Usuario criate(@Valid @RequestBody Usuario usuario) {
-        return userRepository.save(usuario);
+    public ResponseEntity<Usuario> create(@Valid @RequestBody Usuario usuario) {
+        Usuario user = userRepository.save(usuario);
+        return ResponseEntity.status(201).body(user);
     }
 
     @GetMapping("/Listar")
-    public List<Usuario> List() {
+    public List<Usuario> list() {
         return userRepository.findAll();
     }
 
     @GetMapping("/buscar/{id}") //
-    public Usuario find(@PathVariable Long id) {
-        Usuario user =  userRepository.findById(id).get();
-
-        return user;
+    public ResponseEntity<?> buscar(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(t -> ResponseEntity.ok(t))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/update/{id}/{nome}/{idade}") //
-    public Usuario update(Usuario usuario, @PathVariable Long id, @PathVariable String nome, @PathVariable Integer idade) {
-        Usuario newUser = userRepository.findById(id).get();
-        newUser.setNome(nome);
-        newUser.setIdade(idade);
-        return userRepository.save(newUser);
+    public ResponseEntity<?> update(@PathVariable Long id, @PathVariable String nome, @PathVariable Integer idade) {
+            Optional<Usuario> user = userRepository.findById(id);
+
+            if (user.isEmpty()) {
+                return ResponseEntity.status(404).body("Usuario não encontrado!");
+            }
+
+            Usuario userUpdate = user.get();
+            userUpdate.setNome(nome);
+            userUpdate.setIdade(idade);
+            Usuario updatedUser = userRepository.save(userUpdate);
+
+            return ResponseEntity.status(200).body(updatedUser);
     }
 
     @DeleteMapping("/delete/{id}")
-    public Usuario delete(@PathVariable Long id) {
-        Usuario user = userRepository.findById(id).get();
-        userRepository.delete(user);
-        return user;
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        if (!userRepository.existsById(id)) {
+           return ResponseEntity.notFound().build();
+        }
+
+        userRepository.deleteById(id);
+        return ResponseEntity.status(200).build();
     }
 }
